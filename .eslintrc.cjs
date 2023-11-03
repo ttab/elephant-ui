f758e1ff02cbe2e41b134388e52ca4bf3e3b7720
Gustav Larsson <gularsson@users.noreply.github.com>
2023-11-03 11:31:36 +0100

    revert .eslintcjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,10 @@
 module.exports = {
-  settings: {
-    react: {
-      version: 'detect'
-    }
-  },
   root: true,
-  env: {
-    browser: true,
-    es2021: true,
-    jest: true
-  },
+  env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
-    'standard-with-typescript'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
@@ -24,19 +14,5 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
-    "@typescript-eslint/prefer-nullish-coalescing": "off",
-    "@typescript-eslint/strict-boolean-expressions": "off",
-    "no-multiple-empty-lines": ["warn", { max: 2, maxEOF: 1 }],
-    "@typescript-eslint/no-confusing-void-expression": ["error", { ignoreArrowShorthand: true }],
-    "@typescript-eslint/space-before-function-paren": ["warn", {
-        "asyncArrow": "always",
-        "anonymous": "never",
-        "named": "never",
-    }],
-    "space-before-function-paren": ["warn", {
-        "asyncArrow": "always",
-        "anonymous": "never",
-        "named": "never",
-    }]
-  }
+  },
 }
