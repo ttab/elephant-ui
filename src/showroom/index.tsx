046349bb8f88734baefe3a5ea7fcbf43e879c5de
Gustav Larsson <gularsson@users.noreply.github.com>
2024-01-22 13:45:15 +0100

    chore: update deps (#37)

--- src/showroom/index.tsx
+++ src/showroom/index.tsx
@@ -4,7 +4,13 @@ import * as ReactDOM from 'react-dom/client'
 import { App } from './app'
 import '../styles/index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const root = document.getElementById('root')
+
+if (!root) {
+  throw new Error('Can not getElementById("root")')
+}
+
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
