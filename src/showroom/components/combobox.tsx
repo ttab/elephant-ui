d73bc6e12b4cedec5dd3319b90aa777357abf19b
Victor Lindh <victorlindh@hotmail.com>
2024-08-21 23:04:28 +0200

    better combobox showroom variant

--- src/showroom/components/combobox.tsx
+++ src/showroom/components/combobox.tsx
@@ -4,13 +4,13 @@ import {
 
 import { Header } from '../header'
 import { Code } from '../code'
-import { KanbanIcon, PenBoxIcon, UserCheck, UserPlus } from 'lucide-react'
+import { KanbanIcon } from 'lucide-react'
 
 export function ComboBoxExample(): JSX.Element {
   const options = [
-    { value: 'serenawilliams', label: 'Serena Williams', icon: UserPlus },
-    { value: 'andymurray', label: 'Andy Murray', icon: UserCheck },
-    { value: 'petesampras', label: 'Pete Sampras', icon: PenBoxIcon },
+    { value: 'serenawilliams', label: 'Serena Williams', color: 'bg-[#2314aa]' },
+    { value: 'andymurray', label: 'Andy Murray' },
+    { value: 'petesampras', label: 'Pete Sampras', avatar: <div className='w-full h-full bg-gradient-to-r from-cyan-500 to-blue-500 ... rounded-full outline-dotted items-center'/> },
     { value: 'rogerfederer', label: 'Roger Federer', icon: KanbanIcon }
   ]
 
@@ -23,20 +23,28 @@ export function ComboBoxExample(): JSX.Element {
         max={3}
         selectedOption={selectedOption}
         options={options}
-        placeholder='Add an item'
+        placeholder='Add a player'
         onSelect={(option) => console.log(option)}
       >
-        {!selectedOption.length ? <UserPlus /> : null}
       </ComboBox>
     </div>
-    <Code code={`
-      <ComboBox
-        max={3}
-        selectedOption={selectedOption}
-        options={exampleOptions}
-        placeholder='Add an item'
-        onSelect={(option) => console.log(option)}
-      >
-      </ComboBox>`} />
+    <Code code={`const options = [
+  { value: 'serenawilliams', label: 'Serena Williams', color: 'bg-[#2314aa]' },
+  { value: 'andymurray', label: 'Andy Murray' },
+  { value: 'petesampras', label: 'Pete Sampras', avatar: <div className='w-full h-full bg-gradient-to-r from-cyan-500 to-blue-500 ... rounded-full outline-dotted items-center'/> },
+  { value: 'rogerfederer', label: 'Roger Federer', icon: KanbanIcon }
+]
+
+const selectedOption = [options[0]]
+
+<ComboBox
+  max={3}
+  selectedOption={[]}
+  options={options}
+  placeholder='Add a player'
+  onSelect={(option) => console.log(option)}
+>
+</ComboBox>`}
+    />
   </>
 }
