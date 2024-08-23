5bee71fe81ed965c6205cd428d073401e54bc5b7
Victor Lindh <victorlindh@hotmail.com>
2024-08-23 12:45:56 +0200

    bugfix: ComboBox PopOver can't scroll long lists

--- src/components/ui/combobox.tsx
+++ src/components/ui/combobox.tsx
@@ -135,7 +135,7 @@ export function ComboBox({
 
   if (isDesktop) {
     return (
-        <Popover open={open} onOpenChange={handleOpenChange}>
+        <Popover open={open} onOpenChange={handleOpenChange} modal={true}>
           <PopoverTrigger asChild>
             <Button
               size={size}
