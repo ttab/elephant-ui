2a329cb9f51e756a270d121c46234b02f81ca1fe
Danne Lundqvist <danne@dotvoid.com>
2024-10-02 09:42:45 +0200

    Allow setting combobox debounce value, make combobox controlled/uncontrolled mutually exclusive

---
+++ src/components/ui/comboboxTypes.tsx
@@ -0,0 +1,22 @@
+'use client'
+import React, { type Dispatch } from 'react'
+import { type LucideIcon } from 'lucide-react'
+
+export interface DefaultValueOption {
+  payload?: unknown
+  label: string
+  value: string
+  icon?: LucideIcon
+  avatar?: React.ReactNode
+  iconProps?: {
+    size?: number
+    fill?: string
+    color?: string
+    strokeWidth?: number
+    className?: string
+  }
+  color?: string
+  info?: string
+}
+
+export type SortableKeys = 'label' | 'value'
