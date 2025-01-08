d813dd548a334ec7243dee3f5b7088b836faacff
gvl <gustav.larsson@tt.se>
2025-01-08 14:19:13 +0100

    fix: update container breakpoints

--- src/styles/preset.ts
+++ src/styles/preset.ts
@@ -24,6 +24,10 @@ export default {
       center: true,
       padding: '2rem',
       screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
         '2xl': '1536px',
         fhd: '1920px',
         qhd: '2560px',
