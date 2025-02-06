0ec0cc946be00f10e90234ac02a31d22520882f1
Gustav Larsson <gularsson@users.noreply.github.com>
2025-02-06 09:33:59 +0100

    feat: add toggle and toggle-group (#595)

---
+++ src/showroom/components/toggle.tsx
@@ -0,0 +1,21 @@
+import { Code } from '../code'
+import { Bold } from 'lucide-react'
+import { Header } from '../header'
+import { Toggle } from '@/components/ui/toggle'
+
+export const ToggleExample = (): JSX.Element => {
+  return (
+    <>
+      <Header>Toggle</Header>
+      <div>
+        <Toggle aria-label='Toggle bold'>
+          <Bold className='h-4 w-4' />
+        </Toggle>
+      </div>
+      <Code code={`<Toggle aria-label='Toggle bold'>
+  <Bold className='h-4 w-4' />
+</Toggle>`}
+      />
+    </>
+  )
+}
