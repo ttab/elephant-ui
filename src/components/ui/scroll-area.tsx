83159382f9bd104b8b097c5801baef535747b52d
gvl <git@gula.dev>
2025-03-10 12:55:49 +0100

    fix: scroll-area overflow

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -14,7 +14,10 @@ const ScrollArea = React.forwardRef<
     className={cn('relative overflow-hidden', className)}
     {...props}
   >
-    <ScrollAreaPrimitive.Viewport className='h-full w-full rounded-[inherit]'>
+    {/* [&>div]:!block due to:
+    *   https://github.com/radix-ui/primitives/issues/926
+    */}
+    <ScrollAreaPrimitive.Viewport className='h-full w-full rounded-[inherit] [&>div]:!block'>
       {children}
     </ScrollAreaPrimitive.Viewport>
     <ScrollBar />
