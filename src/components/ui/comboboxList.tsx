0925fcd33edfcba13d032f18ac3f45b8a39bb7f9
Gustav Larsson <gularsson@users.noreply.github.com>
2025-04-24 15:11:36 +0200

    fix(Combobox): add keywords, update radix components (#742)

--- src/components/ui/comboboxList.tsx
+++ src/components/ui/comboboxList.tsx
@@ -87,6 +87,7 @@ export function ComboBoxList({
               className='group/checkbox'
               key={option.value}
               value={option.value}
+              keywords={[option.label, option.value]}
               onMouseUp={handleMouseUp}
               onSelect={(value) => {
                 const newSelectedOption = options.find((option) => option.value === value)
@@ -100,7 +101,7 @@ export function ComboBoxList({
             >
               <div className='grid grid-flow-col auto-cols-auto items-center gap-2'>
                 <div className='w-6 p-0.5' data-ischeckbox>
-                  {selectedOptions.find((o) => o.label === option.label)
+                  {selectedOptions.find((o) => o.value === option.value)
                     ? <SquareCheck size={18} strokeWidth={1.75} className='mr-4 group-hover/checkbox:opacity-50' />
                     : <Square size={18} strokeWidth={1.75} className='mr-4 opacity-0 group-hover/checkbox:opacity-50' />}
                 </div>
