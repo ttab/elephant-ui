339a591a8afb8f9cb67a7bfb52a4bef2b429ab16
gvl <git@gula.dev>
2026-02-19 09:45:20 +0100

    chore: coerce eslint version

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,25 @@
 import elephant from '@ttab/eslint-config-elephant'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default [
-  ...elephant
+  ...elephant,
+  {
+    settings: {
+      react: {
+        version: '19'
+      }
+    }
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: __dirname
+      }
+    }
+  }
 ]
