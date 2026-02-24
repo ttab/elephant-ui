2b8dc34370091641ab86ed00b7be7666c94853dc
Gustav Larsson <gularsson@users.noreply.github.com>
2026-02-24 12:38:09 +0100

    working (#969)

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -21,7 +21,7 @@ const DialogOverlay = React.forwardRef<
   <DialogPrimitive.Overlay
     ref={ref}
     className={cn(
-      'fixed inset-0 z-50 bg-black/10',
+      'fixed inset-0 z-50 bg-black/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
       className
     )}
     {...props}
@@ -35,32 +35,31 @@ const DialogContent = React.forwardRef<
     onOpenChangeProvided?: boolean
   }>(({ className, children, onOpenChangeProvided = false, ...props }, ref) => (
   <DialogPortal>
-    <DialogOverlay>
-      <DialogPrimitive.Content
-        autoFocus
-        onKeyDown={(event) => {
-          if (event.key !== 'Escape') {
-            event.stopPropagation()
-          }
-        }}
-        ref={ref}
-        className={cn(
-          'fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg xl:h-auto overflow-y-auto max-h-[92dvh] translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg sm:rounded-lg md:w-full focus-visible:outline-none',
-          className
-        )}
-        {...props}
-      >
-        {children}
+    <DialogOverlay />
+    <DialogPrimitive.Content
+      autoFocus
+      onKeyDown={(event) => {
+        if (event.key !== 'Escape') {
+          event.stopPropagation()
+        }
+      }}
+      ref={ref}
+      className={cn(
+        'fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg',
+        className
+      )}
+      {...props}
+    >
+      {children}
 
-        {onOpenChangeProvided
-          && (
-            <DialogPrimitive.Close className='absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground'>
-              <XIcon className='h-4 w-4' />
-              <span className='sr-only'>Close</span>
-            </DialogPrimitive.Close>
-          )}
-      </DialogPrimitive.Content>
-    </DialogOverlay>
+      {onOpenChangeProvided
+        && (
+          <DialogPrimitive.Close className='absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground'>
+            <XIcon className='h-4 w-4' />
+            <span className='sr-only'>Close</span>
+          </DialogPrimitive.Close>
+        )}
+    </DialogPrimitive.Content>
   </DialogPortal>
 ))
 DialogContent.displayName = DialogPrimitive.Content.displayName
